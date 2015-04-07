fcad88f7852d4f4307910b54aaab9498b0da82d1
Thomas Parslow <tom@almostobsolete.net>
2015-04-07 18:52:34 +0100

    Allow use of the database object before it has finished openning

--- sqlite3.ios.js
+++ sqlite3.ios.js
@@ -9,29 +9,44 @@ function SQLite3Error(message) {
 
 SQLite3Error.prototype = new Error();
 
-function Database(databaseId) {
-  this._databaseId = databaseId;
+function Database(databaseName, openCallback) {
+  this._databaseId = null;
+  // List of actions pending database connection
+  this._pendingActions = [];
+
+  NativeModules.AIBSQLite.openFromFilename(databaseName, (error, databaseId) => {
+    if (error) {
+      error = new SQLite3Error(error);
+      this._failPendingActions(error);
+      return openCallback(error, null);
+    }
+    this._databaseId = databaseId;
+    this._runPendingActions();
+    openCallback(null, this);
+  });
 }
 
 Database.prototype = {
-  executeSQL(sql: string, params: Array, rowCallback: (row: Object) => void, completeCallback: (error: ?SQLite3Error) => void) {
-    var eventName =  "aibSqliteRow:" + (nextId++);
-    var rowHandler = RCTDeviceEventEmitter.addListener(
-      eventName,
-      rowCallback
-    );
+  executeSQL (sql: string, params: Array, rowCallback: (row: Object) => void, completeCallback: (error: ?SQLite3Error) => void) {
+    this._addAction(completeCallback, (callback) => {
+      var eventName =  "aibSqliteRow:" + (nextId++);
+      var rowHandler = RCTDeviceEventEmitter.addListener(
+        eventName,
+        rowCallback
+      );
 
-    NativeModules.AIBSQLite.execOnDatabase(this._databaseId, sql, params, eventName, function (error) {
-      rowHandler.remove();
-      if (error) {
-        completeCallback(new SQLite3Error(error));
-      } else {
-        completeCallback(null);
-      }
+      NativeModules.AIBSQLite.execOnDatabase(this._databaseId, sql, params, eventName, function (error) {
+        rowHandler.remove();
+        if (error) {
+          callback(new SQLite3Error(error));
+        } else {
+          callback(null);
+        }
+      });
     });
   },
 
-  close (callback: ?(error: ?SQLite3Error) => void) {
+  close(callback: ?(error: ?SQLite3Error) => void) {
     NativeModules.AIBSQLite.closeDatabase(this._databaseId, (error) => {
       if (!callback) return;
       if (error) {
@@ -40,17 +55,34 @@ Database.prototype = {
         callback(null);
       }
     });
+  },
+
+  _addAction(callback, action) {
+    if (this._databaseId) {
+      action(callback);
+    } else {
+      this._pendingActions.push({action, callback});
+    }
+  },
+
+  _runPendingActions () {
+    this._pendingActions.forEach(
+       ({action, callback}) => {
+        action(callback);
+      });
+  },
+
+  _failPendingActions (error) {
+    this._pendingActions.forEach(
+      ({action, callback}) => {
+        callback(error);
+      });
   }
 };
 
 module.exports = {
   SQLite3Error: SQLite3Error,
-  open (databaseName: string, callback: (error: ?SQLite3Error, database: ?Database) => void ) {
-    NativeModules.AIBSQLite.openFromFilename(databaseName, (error, databaseId) => {
-      if (error) {
-        return callback(new SQLite3Error(error), null);
-      }
-      callback(null, new Database(databaseId));
-    });
+  open (databaseName: string, callback: ?((error: ?SQLite3Error, database: ?Database) => void) ) {
+    return new Database(databaseName, callback || ((e) => null));
   }
 };
